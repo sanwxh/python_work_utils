cfcbe9a286656603324c1eda6196a921f306a942
sanwxh <sandrov037@gmail.com>
2023-12-23 15:29:13 -0500

    removed pypdf and added pdfplumber

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 # look at the input folder and create a loop for the pdf and a nested loop for the pdf pages
 # extract the sq footage, address, type of job (SH,DI), PO number and price
 # output data to a text document
-import PyPDF2 as pdfS
+
