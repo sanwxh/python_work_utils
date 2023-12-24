c7109f0f6d39936899a7b0eabc5dcd33ea4754f9
sanwxh <sandrov037@gmail.com>
2023-12-24 07:08:09 -0500

    Done

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 import os
 import pdfplumber
 
-def extract_specific_area_from_pdf(pdf_path, region):
+def extract_specific_area_from_pdf(pdf_path, sq_coords, address_coords, type_coords, po_coords, total_coords):
     """
     Extract text from a specific region of each page of a PDF file.
     """
     with pdfplumber.open(pdf_path) as pdf:
         # Extract text from the specified region of each page
-        text_per_page = [page.crop(region).extract_text() for page in pdf.pages]
-    return text_per_page
+        sq_text = [page.crop(sq_coords).extract_text() for page in pdf.pages]
+        address_text = [page.crop(address_coords).extract_text() for page in pdf.pages]
+        type_text = [page.crop(type_coords).extract_text() for page in pdf.pages]
+        po_text = [page.crop(po_coords).extract_text() for page in pdf.pages]
+        total_text = [page.crop(total_coords).extract_text() for page in pdf.pages]
+    return sq_text, address_text, type_text, po_text, total_text
 
-def process_pdfs(input_folder, output_file, region):
+def process_pdfs(input_folder, output_file, sq_coords, address_coords, type_coords, po_coords, total_coords):
     """
     Process all PDFs in the input folder, delete existing output file,
     create a new one, and write parsed data to the new output text file.
@@ -27,19 +31,31 @@ def process_pdfs(input_folder, output_file, region):
                 pdf_path = os.path.join(input_folder, filename)
                 
                 # Extract text from the specified region of each page of the PDF
-                text_per_page = extract_specific_area_from_pdf(pdf_path, region)
+                sq_text, address_text, type_text, po_text, total_text = extract_specific_area_from_pdf(pdf_path, sq_coords, address_coords, type_coords, po_coords, total_coords)
                 
                 # Write the extracted text to the output file
                 output_file.write(f"=== {filename} ===\n")
-                for page_num, text in enumerate(text_per_page, start=1):
-                    output_file.write(f"Page {page_num}:\n{text}\n\n")
+                for page_num, (sq, address, type, po, total) in enumerate(zip(sq_text, address_text, type_text, po_text, total_text), start=1):
+                    output_file.write(f"Page {page_num}:\n")
+                    output_file.write(f"{sq} {address} {type}\nPO# {po}\n${total}\n")
+                    output_file.write("\n")
 
 # Specify input folder, output file, and the region to extract
 input_folder = 'input'
 output_file = 'output.txt'
 # Specify the region to extract (replace with appropriate coordinates)
 # For example: (x0, y0, x1, y1) where (x0, y0) is the top-left corner and (x1, y1) is the bottom-right corner
-region = (498, 78, 542, 95)
+
+# Good Cords
+sq_coords = (428, 335, 461, 348)
+# Bad Cords
+address_coords = (399, 150, 545, 162)
+# Good Cords
+type_coords = (152, 336, 168, 345)
+# Good Cords
+po_coords = (502, 79, 541, 96)
+# Good Cords
+total_coords = (522, 355, 599, 373)
 
 # Process PDFs, delete existing output file, create a new one, and write parsed data to the new output file
-process_pdfs(input_folder, output_file, region)
+process_pdfs(input_folder, output_file, sq_coords, address_coords, type_coords, po_coords, total_coords)
